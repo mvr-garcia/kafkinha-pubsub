4dff265c67649e1648ff688e3e138cdfaf615713
Marcos Garcia <vinicius-garcia13@hotmail.com>
2025-08-19 23:33:15 -0300

    feat(producer): implement kafka producer

--- cmd/producer.go
+++ cmd/producer.go
@@ -1,22 +1,42 @@
 package cmd
 
 import (
+	"strings"
+
+	"github.com/mvr-garcia/kafikinha/pkg/kafka"
 	"github.com/mvr-garcia/kafikinha/pkg/logger"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
 
+var message string
+
 var producerCmd = &cobra.Command{
 	Use:   "producer",
 	Short: "Start the producer",
 	Run: func(cmd *cobra.Command, args []string) {
-		logger.L().Info("starting producer",
-			zap.String("brokers", brokers),
+		brokerList := strings.Split(brokers, ",")
+		p, err := kafka.NewProducer(brokerList, topic)
+		if err != nil {
+			logger.L().Fatal("failed to create producer", zap.Error(err))
+		}
+		defer p.Close()
+
+		if message == "" {
+			logger.L().Warn("no message provided, skipping send")
+			return
+		}
+
+		logger.L().Info("sending message",
 			zap.String("topic", topic),
+			zap.String("message", message),
 		)
+
+		p.SendMessage(message)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(producerCmd)
+	producerCmd.Flags().StringVar(&message, "message", "", "message to send")
 }
---
+++ pkg/kafka/producer.go
@@ -0,0 +1,74 @@
+package kafka
+
+import (
+	"time"
+
+	"github.com/IBM/sarama"
+	"github.com/mvr-garcia/kafikinha/pkg/logger"
+	"go.uber.org/zap"
+)
+
+type Producer struct {
+	asyncProducer sarama.AsyncProducer
+	topic         string
+	done          chan struct{}
+}
+
+func NewProducer(brokers []string, topic string) (*Producer, error) {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+	config.Producer.Idempotent = true
+	config.Net.MaxOpenRequests = 1
+
+	asyncProducer, err := sarama.NewAsyncProducer(brokers, config)
+	if err != nil {
+		return nil, err
+	}
+
+	p := &Producer{
+		asyncProducer: asyncProducer,
+		topic:         topic,
+	}
+
+	// Handle errors and successes asynchronously
+	go func() {
+		for {
+			select {
+			case err := <-asyncProducer.Errors():
+				if err != nil {
+					logger.L().Error("failed to send message", zap.Error(err))
+				}
+			case success := <-asyncProducer.Successes():
+				if success != nil {
+					logger.L().Info("message delivered",
+						zap.String("topic", success.Topic),
+						zap.Int32("partition", success.Partition),
+						zap.Int64("offset", success.Offset),
+					)
+				}
+			case <-p.done:
+				return
+			}
+		}
+	}()
+
+	return p, nil
+}
+
+func (p *Producer) SendMessage(value string) {
+	msg := sarama.ProducerMessage{
+		Topic:     p.topic,
+		Value:     sarama.StringEncoder(value),
+		Timestamp: time.Now(),
+	}
+
+	p.asyncProducer.Input() <- &msg
+}
+
+func (p *Producer) Close() error {
+	close(p.done)
+	return p.asyncProducer.Close()
+}

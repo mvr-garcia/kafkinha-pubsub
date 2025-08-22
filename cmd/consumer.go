c7cbcbacce6083134b221e87799c67a3da892bd3
Marcos Garcia <vinicius-garcia13@hotmail.com>
2025-08-21 23:25:39 -0300

    feat(consumer): add Kafka consumer group implementation

--- cmd/consumer.go
+++ cmd/consumer.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/mvr-garcia/kafikinha/pkg/kafka"
 	"github.com/mvr-garcia/kafikinha/pkg/logger"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
@@ -10,13 +11,25 @@ var consumerCmd = &cobra.Command{
 	Use:   "consumer",
 	Short: "Start the consumer",
 	Run: func(cmd *cobra.Command, args []string) {
-		logger.L().Info("starting consumer",
-			zap.String("brokers", brokers),
-			zap.String("topic", topic),
-		)
+		brokers, _ := cmd.Flags().GetStringSlice("brokers")
+		topic, _ := cmd.Flags().GetString("topic")
+		groupID, _ := cmd.Flags().GetString("group")
+
+		cons, err := kafka.NewConsumer(brokers, topic, groupID)
+		if err != nil {
+			logger.L().Fatal("failed to create consumer", zap.Error(err))
+		}
+
+		cons.Start()
+
+		// Block until termination
+		select {}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(consumerCmd)
+	consumerCmd.Flags().StringSlice("brokers", []string{"localhost:9092"}, "Kafka brokers")
+	consumerCmd.Flags().String("topic", "events", "Kafka topic to consume from")
+	consumerCmd.Flags().String("group", "kafikinha-group", "Kafka consumer group ID")
 }
---
+++ pkg/kafka/consumer.go
@@ -0,0 +1,103 @@
+package kafka
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/IBM/sarama"
+	"github.com/mvr-garcia/kafikinha/pkg/logger"
+	"go.uber.org/zap"
+)
+
+type Consumer struct {
+	group   sarama.ConsumerGroup
+	topic   string
+	groupID string
+	done    chan struct{}
+}
+
+func NewConsumer(brokers []string, topic, groupID string) (*Consumer, error) {
+	config := sarama.NewConfig()
+	config.Version = sarama.V2_5_0_0
+	config.Consumer.Return.Errors = true
+	config.Consumer.Offsets.Initial = sarama.OffsetNewest
+
+	group, err := sarama.NewConsumerGroup(brokers, groupID, config)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Consumer{
+		group:   group,
+		topic:   topic,
+		groupID: groupID,
+		done:    make(chan struct{}),
+	}
+
+	return c, nil
+}
+
+func (c *Consumer) Start() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		defer cancel()
+		for {
+			if err := c.group.Consume(ctx, []string{c.topic}, c); err != nil {
+				logger.L().Error("error consuming", zap.Error(err))
+			}
+
+			if ctx.Err() != nil {
+				return
+			}
+		}
+	}()
+
+	go func() {
+		sigterm := make(chan os.Signal, 1)
+		signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+		select {
+		case <-sigterm:
+			logger.L().Info("shutdown signal received")
+			cancel()
+		case <-c.done:
+			cancel()
+		}
+	}()
+}
+
+func (c *Consumer) Close() {
+	close(c.done)
+	c.group.Close()
+}
+
+// --- sarama.ConsumerGroupHandler implementation ---
+
+func (c *Consumer) Setup(_ sarama.ConsumerGroupSession) error {
+	logger.L().Info("consumer group setup",
+		zap.String("groupID", c.groupID),
+		zap.String("topic", c.topic))
+	return nil
+}
+
+func (c *Consumer) Cleanup(_ sarama.ConsumerGroupSession) error {
+	logger.L().Info("consumer group cleanup",
+		zap.String("groupID", c.groupID),
+		zap.String("topic", c.topic))
+	return nil
+}
+
+func (c *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for message := range claim.Messages() {
+		logger.L().Info("message consumed",
+			zap.String("topic", message.Topic),
+			zap.Int32("partition", message.Partition),
+			zap.Int64("offset", message.Offset),
+			zap.ByteString("value", message.Value),
+		)
+		session.MarkMessage(message, "") // offset commit
+	}
+	return nil
+}
